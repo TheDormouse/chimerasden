942ad1d9f75ab0d5b90fa5f72a03bf12a5ed0e8b
Marchy <37279013+TheDormouse@users.noreply.github.com>
2024-09-10 23:07:07 -0400

    clarity2

--- app/layout.js
+++ app/layout.js
@@ -24,13 +24,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <Script type="text/javascript">
+      <script id="clarity-script" type="text/javascript" strategy="afterInteractive">
     {`(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "o1cbcl5xsv");`}
-</Script>
+</script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <div className="container">
